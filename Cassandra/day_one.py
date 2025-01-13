620f5b93cf784ff0db7ff5e4ace5eedbc17f53b0
Paul Ifemene <ifemenep@gmail.com>
2025-01-13 23:59:06 +0300

    Update day_one.py

--- Cassandra/day_one.py
+++ Cassandra/day_one.py
@@ -1,12 +1,19 @@
 print("cassandra is pretty, but she's a rapist")
 paul = "hello everyone"
 print(paul)
-num_one = 498
-num_two = 678
+num_one = 765
+num_two = 677664
+
+
 total = num_one + num_two
 print(total)
 user_name = input("what is your name: ")
 print("welcome to UCU!!!",user_name)
+
+Paul = "Handsome"
+Yolo = "Cute"
+
+Salim = 
 
 
 
