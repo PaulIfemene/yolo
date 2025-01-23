cebaa7675e836ca8d7a393251cdf892aca68219f
Paul Ifemene <ifemenep@gmail.com>
2025-01-23 22:03:45 +0300

    Update grading_system.py

--- PEACE/grading_system.py
+++ PEACE/grading_system.py
@@ -11,3 +11,15 @@ else:
     
 
 # write a python program to get a user's name and age and determine if their eligible to vote
+
+grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+elif grade >= 50:
+    print("you have grade C")
+else:
+    print("you have grade F")
+    
