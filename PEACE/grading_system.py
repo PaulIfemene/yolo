65315a8a4f112d2621b26af7a9af4d661dc0464d
Paul Ifemene <ifemenep@gmail.com>
2025-07-18 22:53:18 +0300

    Update grading_system.py

--- PEACE/grading_system.py
+++ PEACE/grading_system.py
@@ -95,4 +95,101 @@ if grade >= 80:
 elif grade >= 65: 
     print("you have grade B")
     print("you have grade F")
+    grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+elif grade >= 50:
+    print("you have grade C")
+else:
+    print("you have grade F")
+    
+
+# write a python program to get a user's name and age and determine if their eligible to vote
+
+grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+    print("you have grade F")
+    
+
+
+
+grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+elif grade >= 50:
+    print("you have grade C")
+else:
+    print("you have grade F")
+    
+
+# write a python program to get a user's name and age and determine if their eligible to vote
+
+grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+    print("you have grade F")
+    
+
+
+
+grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+elif grade >= 50:
+    print("you have grade C")
+else:
+    print("you have grade F")
+    
+
+# write a python program to get a user's name and age and determine if their eligible to vote
+
+grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+    print("you have grade F")
+    
+
+
+
+
+grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+elif grade >= 50:
+    print("you have grade C")
+else:
+    print("you have grade F")
+    
+
+# write a python program to get a user's name and age and determine if their eligible to vote
+
+grade = int(input("please enter your exam grades/100: "))
+
+if grade >= 80: 
+    print("you have grade A")
+elif grade >= 65: 
+    print("you have grade B")
+    print("you have grade F")
     
