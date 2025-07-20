85b60a78f61f316e026173affa456670eb0398ae
Paul Ifemene <ifemenep@gmail.com>
2025-07-20 23:06:36 +0300

    Update testing.py

--- PEACE/testing.py
+++ PEACE/testing.py
@@ -62,3 +62,67 @@ elif user_age >= 18:
 else:
     print("Sorry, you can't vote")
 
+user_age = int(input("Please enter your age: "))
+
+if user_age >= 70:
+    print("You are of retirement age")
+elif user_age >= 18:
+    print("You are eligible to vote")
+else:
+    print("Sorry, you can't vote")
+
+
+
+
+user_age = int(input("Please enter your age: "))
+
+if user_age >= 70:
+    print("You are of retirement age")
+elif user_age >= 18:
+    print("You are eligible to vote")
+else:
+    print("Sorry, you can't vote")
+
+
+
+user_age = int(input("Please enter your age: "))
+
+if user_age >= 70:
+    print("You are of retirement age")
+elif user_age >= 18:
+    print("You are eligible to vote")
+else:
+    print("Sorry, you can't vote")
+
+user_age = int(input("Please enter your age: "))
+
+if user_age >= 70:
+    print("You are of retirement age")
+elif user_age >= 18:
+    print("You are eligible to vote")
+else:
+    print("Sorry, you can't vote")
+
+
+
+
+user_age = int(input("Please enter your age: "))
+
+if user_age >= 70:
+    print("You are of retirement age")
+elif user_age >= 18:
+    print("You are eligible to vote")
+else:
+    print("Sorry, you can't vote")
+
+
+
+user_age = int(input("Please enter your age: "))
+
+if user_age >= 70:
+    print("You are of retirement age")
+elif user_age >= 18:
+    print("You are eligible to vote")
+else:
+    print("Sorry, you can't vote")
+
